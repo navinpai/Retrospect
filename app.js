dcdb39fdc16b926f79683d4a4fc8ef26eda5c215
Navin Pai <lifeofnavin@gmail.com>
2013-06-20 02:09:22 +0530

    Initial heroku commit

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ var server = http.createServer(function(req, res) {
 }).listen(8080, function() {
     console.log('Listening at: http://localhost:8080');
 });
+
  
 socketio.listen(server).on('connection', function (socket) {
-    socket.on('message', function (msg) {
+    socket.on('new update', function (msg) {
         console.log('Message Received: ', msg);
-        socket.broadcast.emit('message', msg);
+
+        socket.broadcast.emit('message', {'message':msg.message,'id':msg.id});
+        
+        
     });
 });
